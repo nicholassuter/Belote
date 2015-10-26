337139e4fe7742adc3b54ae25cc5d40692604a71
Nicholas Suter <nicholas.suter@cellenza.com>
2015-10-26 09:32:58 +0100

    Add missing file (thanks git)

---
+++ Belote.Engine.Tests/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Belote.Engine.Tests
+{
+    [TestFixture]
+    public class GameTests
+    {
+        [Test]
+        public void Demarrer_une_nouvelle_partie()
+        {
+            Joueur joueurOuest = new Joueur("Ouest", JoueurType.Ordinateur);
+            Joueur joueurNord = new Joueur("Nord", JoueurType.Ordinateur);
+            Joueur joueurEst = new Joueur("Est", JoueurType.Ordinateur);
+            Joueur nicholas = new Joueur("Nicholas", JoueurType.Humain);
+            Partie partie = new Partie(1000, joueurOuest, joueurNord, joueurEst, nicholas);
+        }
+    }
+
+    
+}
